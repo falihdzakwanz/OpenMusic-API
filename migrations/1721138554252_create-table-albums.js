ec20303cce07259bccb917001843ed048a0b90ee
Falih <falihdzakwan028@gmail.com>
2024-07-16 21:25:06 +0700

    feat: db setup

---
+++ migrations/1721138554252_create-table-albums.js
@@ -0,0 +1,34 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+    pgm.createTable('albums', {
+        id: {
+            type: 'VARCHAR(50)',
+            primaryKey: true,
+        },
+        name: {
+            type: 'VARCHAR(255)',
+            notNull: true,
+        },
+        year: {
+            type: 'INTEGER',
+            notNull: true,
+        },
+    })
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+    pgm.dropTable('albums');
+};
---
+++ migrations/1721138628988_create-table-songs.js
@@ -0,0 +1,59 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+    pgm.createTable('songs', {
+        id: {
+            type: 'VARCHAR(50)',
+            primaryKey: true,
+        },
+        title: {
+            type: 'VARCHAR(255)',
+            notNull: true,
+        },
+        year: {
+            type: 'INTEGER',
+            notNull: true,
+        },
+        performer: {
+            type: 'VARCHAR(255)',
+            notNull: true,
+        },
+        genre: {
+            type: 'VARCHAR(255)',
+            notNull: true,
+        },
+        duration: {
+            type: 'INTEGER',
+            notNull: false,
+        },
+        albumId: {
+            type: 'VARCHAR(50)',
+            notNull: false,
+        },
+    });
+
+    pgm.addConstraint('songs', 'fk_songs.albumId_albums.id', {
+        foreignKeys: {
+            columns: 'albumId',
+            references: 'albums(id)',
+            onDelete: 'CASCADE'
+        }
+    });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+    pgm.dropTable('songs');
+};
