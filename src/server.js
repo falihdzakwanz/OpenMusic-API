76dd653a3eb2dbd83bff96ffb8c1b862060f91d5
Falih <falihdzakwan028@gmail.com>
2024-07-18 17:48:20 +0700

    feat: error onPreResponse 500

--- src/server.js
+++ src/server.js
@@ -43,12 +43,26 @@ const init = async () => {
     server.ext('onPreResponse', (request, h) => {
         const { response } = request;
 
-        if (response instanceof ClientError) {
+        if (response instanceof Error) {
+
+            if (response instanceof ClientError) {
+                const newResponse = h.response({
+                    status: 'fail',
+                    message: response.message,
+                });
+                newResponse.code(response.statusCode);
+                return newResponse;
+            }
+
+            if (!response.isServer) {
+                return h.continue;
+            }
+
             const newResponse = h.response({
-                status: 'fail',
-                message: response.message,
+                status: 'error',
+                message: 'Something went wrong',
             });
-            newResponse.code(response.statusCode);
+            newResponse.code(500);
             return newResponse;
         }
 
